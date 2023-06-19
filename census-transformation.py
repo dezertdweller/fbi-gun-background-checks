59860360100fdb619c1d01c248fc135acbd2ef25
katialopesgilbert <katialopesgilbert@gmail.com>
2023-06-19 15:29:36 -0700

    Removed Fact Note column and removed unwanted Fact Notes rows.

--- census-transformation.py
+++ census-transformation.py
@@ -10,12 +10,30 @@ def load_data():
     df = pd.read_csv('/Users/katialopes-gilbert/data-files/ncis-and-census-data/us-census-data-cleaned.csv')
     return df
 
+def print_head(df):
+    print(df.head())
 
+def print_info(df):
+    print(df.info())
 
+def remove_rows_and_columns(df):
+    # Remove columns
+    df = df.drop(['Fact Note'], axis=1)
+    
+    # Remove rows
+    unwanted_rows = ['FIPS Code', 'nan', 'NOTE: FIPS Code values are enclosed in quotes to ensure leading zeros remain intact.', 
+                     'Value Notes', '1', 'Fact Notes', '(a)', '(b)', '(c)', 
+                     'Value Flags', '-', 'D', 'F', 'FN', 'S', 'X', 'Z']
+    df = df[~df['Fact'].isin(unwanted_rows)]
+    return df
+    
 def save_transformed_data(df):
     df = df.to_csv('/Users/katialopes-gilbert/data-files/ncis-and-census-data/us-census-data-transformed.csv', index=False)
     return df
 
 if __name__ in '__main__':
     df = load_data()
+    df = remove_rows_and_columns(df)
+    print_head(df)
+    print_info(df)
     save_transformed_data(df)
