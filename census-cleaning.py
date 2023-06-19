fb152735a1f89834b162d718f7f445ec447a1eac
katialopesgilbert <katialopesgilbert@gmail.com>
2023-06-19 14:36:42 -0700

    Converted percentage strings into decimal values and floats.

--- census-cleaning.py
+++ census-cleaning.py
@@ -12,13 +12,28 @@ def load_data():
     df = pd.read_csv('/Users/katialopes-gilbert/data-files/ncis-and-census-data/us-census-data.csv')
     return df
 
+def print_head(df):
+    print(df.head())
+
 def remove_commas(df):
     for col in df.columns[1:]: #Exclude 'Fact' column
         df[col] = df[col].str.replace(',', '')
     return df
 
-def print_head(df):
-    print(df.head())
+def convert_percentages(x):
+     if isinstance(x, str):
+        x = x.strip()
+        if "%" in x:
+            if x.startswith("-"):
+                return -float(x.replace("-", "").replace("%", "")) / 100
+            else:
+                return float(x.replace("%", "")) / 100
+     return x
+
+"""This function is designed to operate on an individual cell of the DataFrame, 
+not the entire DataFrame at once. To convert all percentages in the DataFrame, 
+I need to  apply this function to each cell in the DataFrame, which can be done 
+with the applymap() function in if __name..."""
 
 def save_cleaned_data(df):
     df = df.to_csv('/Users/katialopes-gilbert/data-files/ncis-and-census-data/us-census-data-cleaned.csv', index=False)
@@ -27,8 +42,9 @@ def save_cleaned_data(df):
 if __name__ in '__main__':
     df = load_data()
     rc = remove_commas(df)
-    print_head(rc)
-    save_cleaned_data(rc)
+    cp = df.applymap(convert_percentages)
+    print_head(cp)
+    save_cleaned_data(cp)
 
 
 
